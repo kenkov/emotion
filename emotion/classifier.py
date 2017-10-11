65f0612a9f508b767fe239d81be3a348e38411e1
kenkov <kenkovtan@gmail.com>
2017-10-11 14:22:19 +0900

    First commit

---
+++ emotion/classifier.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+
+class PosNegClassifier:
+    def __init__(self, filename):
+        """"""
+        self._label_word_dict, \
+            self._word_label_dict, \
+            self._longest_word_len = self._load_dict(filename)
+
+    def _load_dict(self, filename):
+        label_word_dic = {"p": set(),
+                          "n": set(),
+                          "e": set()}
+        word_label_dic = dict()
+        longest_word_len = -1
+        with open(filename) as f:
+            for line in f:
+                label, word = line.strip("\n").split("\t")
+                label_word_dic[label].add(word)
+                word_label_dic[word] = label
+                len_word = len(word.split(" "))
+                if len_word > longest_word_len:
+                    longest_word_len = len_word
+        return label_word_dic, word_label_dic, longest_word_len
+
+    def predict(self, text):
+        """
+        Args:
+            text (str): 原型で分かち書きされた文
+
+        Returns:
+            int:
+            ポジティブであれば正の値を、ネガティブであれば負の値を、ニュートラルであれば
+            0 を返す。絶対値が大きいほど、程度が大きいことを表す
+        """
+
+        words = text.split(" ")
+        len_words = len(words)
+        score = 0
+        for i in range(self._longest_word_len):
+            lst = [words[j:j+i+1] for j in range(len_words - i)]
+            for word in lst:
+                st = " ".join(word)
+                _label = self._word_label_dict.get(st, "e")
+                if _label == "p":
+                    score += 1
+                elif _label == "n":
+                    score -= 1
+        return score
---
+++ test/test.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+
+import os
+from emotion.classifier import PosNegClassifier
+
+
+def test_PosNegClassifier():
+    dictpath = os.path.join(os.path.dirname(__file__),
+                            "data/emotion.dict")
+    classifier = PosNegClassifier(dictpath)
+
+    text = "学校 が 楽しい"
+    res = classifier.predict(text)
+    assert res == 1
+
+    # 元文「仕事が忙しくてうつ状態になった」
+    text = "仕事 が 忙しい て うつ 状態 に なる た"
+    res = classifier.predict(text)
+    assert res == -1
+
+    # 元文「楽しくて魅力的だ」
+    text = "楽しい て 魅力 的 だ"
+    res = classifier.predict(text)
+    assert res == 2
+
+    # 元文「楽しい時とうつ状態のときがある」
+    text = "楽しい 時 と うつ 状態 の とき が ある"
+    res = classifier.predict(text)
+    assert res == 0
